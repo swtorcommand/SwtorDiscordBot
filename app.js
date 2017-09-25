51737049ef721eb0f01df20bd02e877a8c852254
Derek Martinez <martinezderek2010@gmail.com>
2017-09-25 15:21:40 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 
 // Let's extend the client object with these.
 client.config = require("./config/config.json");
-client.commands = require("./config/commands.json"");
+client.commands = require("./config/commands.json");
 
 client.getRoleByName = function (guild, rank) {
     return client.guilds.get(guild).roles.find(role => String(role.name).toLowerCase() === rank);
